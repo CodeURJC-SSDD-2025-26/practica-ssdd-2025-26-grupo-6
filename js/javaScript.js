d25cedeb38bed0ac17684903221b7cc5615c1724
ma.maccarrone.2023 <matymacca@hotmail.com>
2026-02-23 17:03:36 +0100

    js for login, sign up and load content of movies, series and lists

--- js/javaScript.js
+++ js/javaScript.js
@@ -40,7 +40,17 @@ const listas = [
 ];
 
 //LOGIN FUNCTIONALITY
-document.getElementById('loginButton').addEventListener('click', login);
+document.getElementById('loginButton')?.addEventListener('click', login);
+document.getElementById('resetPass')?.addEventListener('click', resetPassword);
+
+function resetPassword() {
+    const myModal = document.getElementById('myModal')
+    const myInput = document.getElementById('myInput')
+
+    myModal.addEventListener('shown.bs.modal', () => {
+        myInput.focus()
+    })
+}
 
 function login() {
 
@@ -49,16 +59,31 @@ function login() {
 
     let expRegEmail = /[\w-.]+@[\w-]+.[a-z]/;
     //check correctness of username and password
-    if(!(email && password)){
+    if (!(email && password)) {
         alert('Correo electrónico y/o contraseña vacío');
-    }else if(!expRegEmail.test(email)){
+    } else if (!expRegEmail.test(email)) {
         alert('Correo electrónico no valido');
-    }else {  //redirect to principal.html
+    } else {  //redirect to principal.html
         document.location.href = 'principal.html';
     }
 
     //check on bbdd 
+}
+
+//SIGN UP FUNCTIONALITY
+document.getElementById('signUpBtn')?.addEventListener('click', signUp);
+
+function signUp() {
+
+    let username = document.getElementById('floatingInput').value;
+    let password = document.getElementById('floatingPassword').value;
+    let repeatPassword = document.getElementById('floatingPassword2').value;
+    let birthYear = document.getElementById('floatingBirthYear').value;
+
     
+
+
+
 }
 //LOAD CONTENT
 function cargarContenido(datos) {
@@ -78,7 +103,7 @@ function cargarContenido(datos) {
     containers.forEach(div => div.innerHTML = htmlContent);
 }
 
-//DEPENDS ON THE PAGE, LOAD THE CONTENT OF THE FILMOGRAPHY
+//depends on the page, load the content of peliculas, series or listas
 document.addEventListener('DOMContentLoaded', () => {
 
     if (document.getElementById('page-peliculas')) {
@@ -98,8 +123,8 @@ document.addEventListener('DOMContentLoaded', () => {
 
 
 //SEARCH FUNCTIONALITY
-document.getElementById('searchInput').addEventListener('keypress', function (k){ if(k.key === 'Enter'){ search();} });
-document.getElementById('searchBtn').addEventListener('click', search);
+document.getElementById('searchInput')?.addEventListener('keypress', function (k) { if (k.key === 'Enter') { search(); } });
+document.getElementById('searchBtn')?.addEventListener('click', search);
 
 function search() {
 
@@ -117,18 +142,18 @@ function search() {
                 <div class="col"><h2>Resultado de búsqueda:</h2></div>
             </div>`;
 
-        if(filteredPeliculas.length >0 ){
-                html.innerHTML += `<h3>Películas:</h3>
+        if (filteredPeliculas.length > 0) {
+            html.innerHTML += `<h3>Películas:</h3>
                 <div class="result" id="searchResultPeliculas">
                 </div>`;
         }
-        if(filteredSeries.length >0 ){
-                html.innerHTML += `<h3>Series:</h3>
+        if (filteredSeries.length > 0) {
+            html.innerHTML += `<h3>Series:</h3>
                 <div class="result" id="searchResultSeries">
                 </div>`;
         }
-        if(filteredListas.length >0 ){
-                html.innerHTML += `<h3>Listas:</h3>
+        if (filteredListas.length > 0) {
+            html.innerHTML += `<h3>Listas:</h3>
                 <div class="result" id="searchResultListas">
                 </div>`;
         }
