d3d2355a1a59484d6f16bd0a35c57e93410c377c
Carretero2005 <a.carreterob.2023@alumnos.urjc.es>
2026-02-23 01:12:53 +0100

    Finish profile

--- js/avatar.js
+++ js/avatar.js
@@ -1,17 +1,30 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const selector = document.getElementById('selectAvatar');
+    /*const selector = document.getElementById('selectAvatar');*/
     const preview = document.getElementById('previewAvatar');
     const btnGuardar= document.getElementById('saveAvatar');
+    const galleryImages = document.querySelectorAll('.img-selectable');
     /*const allTheAvatars = document.querySelectorAll('.user-avatar');*/
+    const avatarLogo = document.getElementById('avatarLogo');
+    const avatarProfile = document.getElementById('avatarProfile');
+    let selectedScr = "images/perfilNoReg.jpg";
+    galleryImages.forEach(IMG => {
+        IMG.addEventListener('click',function(){
+            galleryImages.forEach(i => i.classList.remove('active-avatar'));
 
-    selector.addEventListener('change', function() {
-        preview.src = this.value
-    });
+            this.classList.add('active-avatar');
+            selectedSrc =this.getAttribute('data-value');
+            preview.src =selectedSrc;
+
+        })
+    })
 
     btnGuardar.addEventListener('click', function() {
-        const newAvatar = selector.value;
-        avatarLogo.src =newAvatar;
-        avatarProfile.src = newAvatar;
+        if(avatarLogo){
+            avatarLogo.src = selectedSrc;
+        }
+        if(avatarProfile){
+            avatarProfile.src = selectedSrc;
+        }
         });
 /*
         document.getElementById('mainProfileAvatar').src = newAvatar;
