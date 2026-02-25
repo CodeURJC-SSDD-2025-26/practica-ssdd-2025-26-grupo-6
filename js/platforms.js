77e5cdc5dc04c42490edea21d8b34345c8a6dc97
Carla Garcia <carlagrsmm@gmail.com>
2026-02-25 19:02:57 +0100

    A lot of changes have been made to the project. Here is a summary of the changes that have been made: - The file `css/styles.css` has been modified to make all the cards look the same and to make the buttons look better. - The file `detallesReseñas.html` has been modified. - A new file `images/peliculaAdministrador.jpg` has been added. - The file `modifyIncludeMovie.html` has been renamed to `includeMovie.html`. - The file `modifyIncludeSerie.html` has been renamed to `includeSerie.html`. - The file `js/genres.js` has been modified to add a new genre. - The file `js/platforms.js` has been modified to add a new platform. - A new file `modifyMovie.html` has been added. - A new file `modifySeries.html` has been added. - The file includeMovie.html has been modified to change the way the genres and platforms are added and the form is presented. - The file includeSerie.html has been modified to change the way the genres and platforms are added and the form is presented.

--- js/platforms.js
+++ js/platforms.js
@@ -6,40 +6,32 @@
   }
 
   function init() {
-    const container = document.getElementById('platformsContainer');
+    const container = document.getElementById('camposPlataformas');
     const addBtn = document.getElementById('addPlatform');
     const removeBtn = document.getElementById('removePlatform');
 
     if (!container || !addBtn || !removeBtn) return;
 
-   const doubleRow = document.querySelector('#platformsContainer .double-row');
-
     addBtn.addEventListener('click', () => {
       const nuevo = crearCampoPlataforma();
-      container.insertBefore(nuevo, doubleRow);
+      container.appendChild(nuevo);
       nuevo.querySelector('input').focus();
     });
 
     removeBtn.addEventListener('click', () => {
       const campos = container.querySelectorAll('.platform-extra');
       if (campos.length > 1) {
-        container.removeChild(campos[campos.length - 1]);
+        campos[campos.length - 1].remove();
       }
     });
-
   }
-  
+
   function crearCampoPlataforma() {
     const wrapper = document.createElement('div');
-    wrapper.className = 'platform-extra';
-
+    wrapper.className = 'input-group platform-extra';
     wrapper.innerHTML = `
-      <div class="form-floating mb-1">
-          <input type="text" class="form-control" name="platforms[]" placeholder="Plataformas disponibles">
-          <label class="text-dark administer-form">Plataformas disponibles</label>
-      </div>
+      <input type="text" class="form-control" name="platforms[]" placeholder="Plataforma">
     `;
-
     return wrapper;
   }
 })();
