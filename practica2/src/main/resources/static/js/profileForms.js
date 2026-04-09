c671d7f5a45e8ecf01f2a8d3824091b2037a910b
Carretero2005 <a.carreterob.2023@alumnos.urjc.es>
2026-04-09 09:31:49 +0200

    Change lines

--- practica2/src/main/resources/static/js/profileForms.js
+++ practica2/src/main/resources/static/js/profileForms.js
@@ -38,10 +38,6 @@ document.addEventListener('DOMContentLoaded', function () {
     /* Inserted the Html and converted to form */
     btnEditProfile.addEventListener('click', function () {
 
-        /*
-        const parts= yearBirthElement.textContent.trim().split('-');
-        const formatedDate = `${parts[2]}-${parts[1]}-${parts[0]}`;
-*/
         usernameElement.innerHTML = `<input type="text" id="usernameInput" class="custom-profile-input" value="${usernameElement.textContent.trim()}">`;
         emailElement.innerHTML = `<input type="email" id="emailInput" class="custom-profile-input" value="${emailElement.textContent.trim()}">`;
         yearBirthElement.innerHTML = `<input type="date" id="yearBirthInput" class="custom-profile-input" value="${yearBirthElement.textContent.trim()}">`;
@@ -56,17 +52,11 @@ document.addEventListener('DOMContentLoaded', function () {
         const newUsername = document.getElementById('usernameInput').value;
         const newEmail = document.getElementById('emailInput').value;
         const newYearBirth = document.getElementById('yearBirthInput').value;
-        /*
-        const partsFinal= newYearBirth.trim().split('-');
-        const formatedDateFinal = `${partsFinal[2]}-${partsFinal[1]}-${partsFinal[0]}`;
-*/
+  
         inputName.value = newUsername;
         inputEmail.value = newEmail;
         inputBirth.value = newYearBirth;
 
-/*        
-        btnEditProfile.style.display = 'inline-block';
-        btnSave.style.display = 'none';*/
         profileForm.submit();
     })
 });             
