80a6b9ff82640dbf43c0bab9f210f6e059c28cac
Carla Garcia <carlagrsmm@gmail.com>
2026-04-11 11:07:38 +0200

    including spring security

--- practica2/src/main/java/es/code/urjc/practica2/repository/AccountRepository.java
+++ practica2/src/main/java/es/code/urjc/practica2/repository/AccountRepository.java
@@ -8,7 +8,6 @@ import es.code.urjc.practica2.model.Account;
 
 
 public interface AccountRepository extends JpaRepository<Account, Long>{
-
     Optional<Account> findByAccountEmail(String accountEmail);
 
     Optional<Account> findByAccountName(String accountName);
---
+++ practica2/src/main/java/es/code/urjc/practica2/security/RepositoryUserDetailsService.java
@@ -0,0 +1,36 @@
+package es.code.urjc.practica2.security;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import es.code.urjc.practica2.model.Account;
+import es.code.urjc.practica2.repository.AccountRepository;;
+
+@Service
+public class RepositoryUserDetailsService implements UserDetailsService {
+
+	@Autowired
+	private AccountRepository accountRepository;
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+		Account account = accountRepository.findByAccountName(username)
+				.orElseThrow(() -> new UsernameNotFoundException("User not found"));
+
+		List<GrantedAuthority> roles = new ArrayList<>();
+		roles.add(new SimpleGrantedAuthority("ROLE_" + account.getAccountRole().name()));
+
+		return new org.springframework.security.core.userdetails.User(account.getAccountName(), 
+				account.getAccountPassword(), roles);
+
+	}
+}
