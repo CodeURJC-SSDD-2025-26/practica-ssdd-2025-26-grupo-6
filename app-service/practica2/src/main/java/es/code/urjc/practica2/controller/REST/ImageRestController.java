e4e2e26248fefd6406dff6886b54721ea7f5aa7f
Carla Garcia <carlagrsmm@gmail.com>
2026-05-06 14:33:21 +0200

    erase unnecesary atuwired

--- app-service/practica2/src/main/java/es/code/urjc/practica2/controller/REST/ImageRestController.java
+++ app-service/practica2/src/main/java/es/code/urjc/practica2/controller/REST/ImageRestController.java
@@ -1,23 +1,15 @@
 package es.code.urjc.practica2.controller.rest;
 
-import java.io.IOException;
 import java.sql.Blob;
-import java.sql.SQLException;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.io.Resource;
-import org.springframework.http.MediaType;
-import org.springframework.http.MediaTypeFactory;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.multipart.MultipartFile;
 
-import es.code.urjc.practica2.dto.ImageDto;
 import es.code.urjc.practica2.model.Image;
 import es.code.urjc.practica2.service.ImageService;
 
