c5072db2f24eb9fef4c35aafb8f8a052548e248f
Carla Garcia <carlagrsmm@gmail.com>
2026-05-06 16:36:15 +0200

    added and modifies methods to imageRestController

--- app-service/practica2/src/main/java/es/code/urjc/practica2/controller/REST/ImageRestController.java
+++ app-service/practica2/src/main/java/es/code/urjc/practica2/controller/REST/ImageRestController.java
@@ -1,33 +1,52 @@
 package es.code.urjc.practica2.controller.rest;
 
-import java.sql.Blob;
+import java.io.IOException;
+import java.sql.SQLException;
 
 import org.springframework.beans.factory.annotation.Autowired;
-
+import org.springframework.core.io.Resource;
+import org.springframework.http.MediaType;
+import org.springframework.http.MediaTypeFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
-import es.code.urjc.practica2.model.Image;
+import es.code.urjc.practica2.dto.ImageDto;
+import es.code.urjc.practica2.mapper.ImageMapper;
 import es.code.urjc.practica2.service.ImageService;
 
 @RestController
-@RequestMapping("/api/v1/img")
+@RequestMapping("/api/v1/images")
 public class ImageRestController {
 	@Autowired
-	private ImageService imageService;
+    private ImageService imageService;
 
-	@GetMapping("/{id}")
-	public ResponseEntity<byte[]> getImage(@PathVariable Long id) throws Exception {
-		Image img = imageService.findById(id);
+    @Autowired
+    private ImageMapper mapper;
 
-		Blob blob = img.getImageFile();
-		byte[] bytes = blob.getBytes(1, (int) blob.length());
+    @GetMapping("/{id}")
+    public ImageDto getImage(@PathVariable long id) {
+        return mapper.toDTO(imageService.getImage(id));
+    }
 
-		return ResponseEntity.ok()
-				.header("Content-Type", "image/jpg") 
-				.body(bytes);
-	}
+    @GetMapping("/{id}/media")
+    public ResponseEntity<Object> getImageFile(@PathVariable long id)
+            throws SQLException, IOException {
+
+        Resource imageFile = imageService.getImageFile(id);
+
+        MediaType mediaType = MediaTypeFactory
+                .getMediaType(imageFile)
+                .orElse(MediaType.IMAGE_JPEG);
+
+        return ResponseEntity
+                .ok()
+                .contentType(mediaType)
+                .body(imageFile);
+    }
 }
